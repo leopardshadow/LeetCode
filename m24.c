dd61db9a03844c8fe6d2630793c181acf4b8dd77
leopardshadow <thurtu811@gmail.com>
2021-04-12 00:31:23 +0800

    24. Swap Nodes in Pairs

---
+++ m24.c
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+
+
+struct ListNode* swapPairs(struct ListNode* head){
+
+    if(!head) return NULL;
+    struct ListNode *first  = head,
+                    *second = head->next;
+    int tmp;
+    while(first && second) {
+        // swap value of first and second
+        tmp = first->val;
+        first->val = second->val;
+        second->val = tmp;
+
+        // update value
+        if((first = second->next) == NULL || (second = first->next) == NULL)
+            break;
+    }
+    return head;
+}
+
+/*
+Runtime: 0 ms, faster than 100.00% of C online submissions for Swap Nodes in Pairs.
+Memory Usage: 6 MB, less than 23.22% of C online submissions for Swap Nodes in Pairs.
+*/
