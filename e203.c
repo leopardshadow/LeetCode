efa31786874c583a63262b228c8be13d9b48ce00
leopardshadow <thurtu811@gmail.com>
2021-04-12 15:58:54 +0800

    add 203. Remove Linked List Elements

---
+++ e203.c
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+
+
+struct ListNode* removeElements(struct ListNode* head, int val){
+
+
+
+    return head;
+}
