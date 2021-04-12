f868dbb2224a00be6e94d8cec64410a10c3cfde6
leopardshadow <thurtu811@gmail.com>
2021-04-12 17:00:18 +0800

    update

--- e203.c
+++ e203.c
@@ -17,7 +17,25 @@ struct ListNode {
 
 struct ListNode* removeElements(struct ListNode* head, int val){
 
+    struct ListNode leading_node;
+    leading_node.next = head;
 
+    struct ListNode *prev = &leading_node,
+                    *curr = head;
 
-    return head;
+    while(curr) {
+        // process
+        if(curr->val == val) {
+            prev->next = curr->next;
+            curr = curr->next;
+        }
+        else {
+            // iter
+            prev = curr;
+            curr = curr->next;
+        }
+        
+    }
+
+    return leading_node.next;
 }
