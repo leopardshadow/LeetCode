b2dd318fcf41ee662f36a2887f91492b2a99730b
leopardshadow <thurtu811@gmail.com>
2021-04-12 16:53:46 +0800

    add 160 interesting solution

--- e160.c
+++ e160.c
@@ -34,3 +34,19 @@ struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *he
 Runtime: 36 ms, faster than 55.44% of C online submissions for Intersection of Two Linked Lists.
 Memory Usage: 13.8 MB, less than 78.34% of C online submissions for Intersection of Two Linked Lists.
 */
+
+
+// COOL solution from https://leetcode.com/problems/intersection-of-two-linked-lists/discuss/1092898/JS-Python-Java-C%2B%2B-or-Easy-O(1)-Extra-Space-Solution-w-Visual-Explanation
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+
+        struct ListNode *a = headA, *b = headB;
+        while (a != b) {
+            a = !a ? headB : a->next;
+            b = !b ? headA : b->next;
+        }
+        return a;
+}
+/*
+Runtime: 36 ms, faster than 55.44% of C online submissions for Intersection of Two Linked Lists.
+Memory Usage: 14.1 MB, less than 11.68% of C online submissions for Intersection of Two Linked Lists.
+*/
