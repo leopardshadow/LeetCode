42043cd376e719f5a43318e55c13ab47d602c4ba
leopardshadow <thurtu811@gmail.com>
2021-04-22 16:45:22 +0800

    update 41. First Missing Positive

--- h41.c
+++ h41.c
@@ -11,6 +11,18 @@ int firstMissingPositive(int* nums, int numsSize){
 }
 
 /*
+the constraint for nums.length:
+1 <= nums.length <= 300
+
+so the answer ranges from 1 to 301
+(1 - smallest positive number, if 1 not presents in the array, answer is 1;
+301 - only 300 numbers, if they are 1..300, 301 is the biggest answer would be)
+
+hence, only needs a record array of size 302 (0- 301)
+*/
+
+
+/*
 Runtime: 0 ms, faster than 100.00% of C online submissions for First Missing Positive.
 Memory Usage: 5.9 MB, less than 51.82% of C online submissions for First Missing Positive.
 */
