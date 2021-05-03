efb5fc7d91eb28b7ec2298b17f0eeb234f3cc630
leopardshadow <thurtu811@gmail.com>
2021-05-03 13:28:44 +0800

    add 94. Binary Tree Inorder Traversal

--- m94.c
+++ m94.c
@@ -1,24 +1,38 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     struct TreeNode *left;
- *     struct TreeNode *right;
- * };
- */
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+#include <stdlib.h>
 
 
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
+int calSize(struct TreeNode* root) {
+    if(!root)
+        return 0;
+    else
+        return calSize(root->left) + calSize(root->right) + 1;
+}
 
-#include <stdio.h>
+
+void fillArray(struct TreeNode* root, int *arr, int *i) {
+    if(root) {
+        fillArray(root->left, arr, i);
+        arr[*i] = root->val;
+        (*i) += 1;
+        fillArray(root->right, arr, i);
+    }
+}
 
 int* inorderTraversal(struct TreeNode* root, int* returnSize){
-    
+    *returnSize = calSize(root);
+    int *retArray = malloc(sizeof(int)*(*returnSize));
+    int i = 0;
+    fillArray(root, retArray, &i);
+    return retArray;
 }
 
-int main() {
-
-
-}
+/*
+Runtime: 0 ms, faster than 100.00% of C online submissions for Binary Tree Inorder Traversal.
+Memory Usage: 5.7 MB, less than 95.61% of C online submissions for Binary Tree Inorder Traversal.
+*/
