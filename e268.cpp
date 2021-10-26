74d5e383dfe77561e3c11e280afcd77b6a310d98
leopardshadow <thurtu811@gmail.com>
2021-10-26 10:10:18 +0800

    add 268. Missing Number

---
+++ e268.cpp
@@ -0,0 +1,34 @@
+/*
+[9,6,4,2,3,5,7,0,1]  size = 9
+*/
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        // int n;
+        // if(num.size() == 0) return 1;
+        // else
+        int n = nums.size() + 1;
+        
+        // 1 0 0 .. 0 -> m 0s
+        int m = 0;
+        while(n) {
+            m++;
+            n = n >> 1;
+        }
+        
+        int ans = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            ans ^= nums[i];
+        }
+        for(int i = nums.size() + 1; i < (1<<m); i++) {
+            ans ^= i;
+        }
+        return ans;   
+    }
+};
+
+/*
+Runtime: 8 ms, faster than 99.81% of C++ online submissions for Missing Number.
+Memory Usage: 18.1 MB, less than 18.80% of C++ online submissions for Missing Number.
+*/
