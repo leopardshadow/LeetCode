b1b2dda6c882600f7f0253fcd1f706a6ddee11cc
leopardshadow <thurtu811@gmail.com>
2021-10-26 21:30:47 +0800

    update 54. other's solution

--- m54.c
+++ m54.c
@@ -47,3 +47,43 @@ public:
 Runtime: 0 ms, faster than 100.00% of C++ online submissions for Spiral Matrix.
 Memory Usage: 6.9 MB, less than 73.12% of C++ online submissions for Spiral Matrix.
 */
+
+
+/*
+下面是討論區裡的解答，他的 direction 轉變的方式還滿有趣的
+巧妙得避掉了每次更新界線時的檢查
+direction 轉換那邊是 FSM
+*/
+/*
+        int direction=1;
+        while(left<=right && top<=bottom)
+        {
+            if(direction==1)
+            {
+                for(int i=left;i<=right;i++) v.push_back(matrix[top][i]);
+                direction=2;
+                top++;
+            }
+            
+            else if(direction==2)
+            {
+                for(int i=top;i<=bottom;i++) v.push_back(matrix[i][right]);
+                direction=3;
+                right--;
+            }
+            
+            else if(direction==3)
+            {
+                for(int i=right;i>=left;i--) v.push_back(matrix[bottom][i]);
+                direction=4;
+                bottom--;
+            }
+            
+            else if(direction==4)
+            {
+                for(int i=bottom;i>=top;i--) v.push_back(matrix[i][left]);
+                direction=1;
+                left++;
+            }
+        }
+*/
