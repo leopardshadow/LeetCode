cb56fc87b326870dcc4473adb65b562b2b6b7ed7
leopardshadow <thurtu811@gmail.com>
2021-10-26 20:27:45 +0800

    add 371. recursive

--- m371.cpp
+++ m371.cpp
@@ -20,16 +20,32 @@ steps: 1 1 1 1 ... 1 1 0 (xor)
        1 1 1 1 ... 0 0 0
        0 0 0 ... 1 0 0 0
 */
-class Solution {
-public:
-    int getSum(int a, int b) {
-        if(!a) return b;
-        // carry, sum
-        return getSum((int)((unsigned)(a&b)<<1), a^b);
-    }
-};
+// class Solution {
+// public:
+//     int getSum(int a, int b) {
+//         if(!a) return b;
+//         // carry, sum
+//         return getSum((int)((unsigned)(a&b)<<1), a^b);
+//     }
+// };
 
 /*
 Runtime: 0 ms, faster than 100.00% of C++ online submissions for Sum of Two Integers.
 Memory Usage: 6 MB, less than 29.60% of C++ online submissions for Sum of Two Integers.
 */
+
+
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int carry, sum;
+        do {
+            carry = (int)((unsigned)(a&b)<<1);
+            sum = a ^ b;
+            a = carry;
+            b = sum;
+        } while(a);
+        return b;
+    }
+};
