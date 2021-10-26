b71a60e56ac353f968428b5dabbab4ff81994e95
leopardshadow <thurtu811@gmail.com>
2021-10-26 20:24:09 +0800

    add 371. Sum of Two Integers

---
+++ m371.cpp
@@ -0,0 +1,35 @@
+/*
+    1 1 ------ carry
+  0 1 1 0 1 -- a
++ 0 0 1 1 0 -- b
+------------
+  1 0 0 1 1     
+  
+  
+  -1     1 1 1 1 1 ... 1 
+   1  +  0 0 0 0 0 ... 1
+---------------------------
+       1 0 0 0 0 0 ... 0
+---------------------------
+steps: 1 1 1 1 ... 1 1 0 (xor)
+       0 0 0 0 ... 0 1 0 (carry)
+---------------------------
+       1 1 1 1 ... 1 0 0
+       0 0 0 0 ... 1 0 0
+---------------------------
+       1 1 1 1 ... 0 0 0
+       0 0 0 ... 1 0 0 0
+*/
+class Solution {
+public:
+    int getSum(int a, int b) {
+        if(!a) return b;
+        // carry, sum
+        return getSum((int)((unsigned)(a&b)<<1), a^b);
+    }
+};
+
+/*
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Sum of Two Integers.
+Memory Usage: 6 MB, less than 29.60% of C++ online submissions for Sum of Two Integers.
+*/
