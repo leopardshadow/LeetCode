288852c7e52480db09ab8143e583c4e8114ecb9a
leopardshadow <thurtu811@gmail.com>
2021-10-26 12:08:01 +0800

    add 273. another approach

--- e217.cpp
+++ e217.cpp
@@ -1,20 +1,39 @@
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         map <int, bool> m;
+//         for(const auto n : nums) {
+//             // there is duplicate
+//             if(m.find(n) != m.end())
+//                 return true;
+//             // no duplicate number
+//             else
+//                 m[n] = true;
+//         }
+//         return false;
+//     }
+// };
+
+/*
+Runtime: 48 ms, faster than 33.31% of C++ online submissions for Contains Duplicate.
+Memory Usage: 21 MB, less than 30.71% of C++ online submissions for Contains Duplicate.
+*/
+
+// another approach: sort!
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        map <int, bool> m;
-        for(const auto n : nums) {
-            // there is duplicate
-            if(m.find(n) != m.end())
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size() - 1; i++) {
+            if(nums[i] == nums[i+1])
                 return true;
-            // no duplicate number
-            else
-                m[n] = true;
         }
         return false;
     }
 };
 
 /*
-Runtime: 48 ms, faster than 33.31% of C++ online submissions for Contains Duplicate.
-Memory Usage: 21 MB, less than 30.71% of C++ online submissions for Contains Duplicate.
+Runtime: 26 ms, faster than 79.12% of C++ online submissions for Contains Duplicate.
+Memory Usage: 15.3 MB, less than 99.99% of C++ online submissions for Contains Duplicate.
 */
