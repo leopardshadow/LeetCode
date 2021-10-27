1bde649a45df5f70360ad1f8a7ed9baf197e6a38
leopardshadow <thurtu811@gmail.com>
2021-10-27 21:26:00 +0800

    update 49. Group Anagrams (unordered_map vs. map)

--- m49.cpp
+++ m49.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string, vector<string> > m;
+        unordered_map<string, vector<string> > m;
         vector<vector<string> > ans;
         string ss;
         for(const auto s : strs) {
@@ -9,15 +9,22 @@ public:
             sort(ss.begin(), ss.end());
             m[ss].push_back(s);
         }
-        for(map<string, vector<string> >::iterator it = m.begin(); it != m.end(); it++) {
-            ans.push_back(it->second);
+        for(const auto mm : m) {
+            ans.push_back(mm.second);
         }
         
         return ans;
     }
 };
 
+// Use map
 /*
 Runtime: 36 ms, faster than 72.98% of C++ online submissions for Group Anagrams.
 Memory Usage: 19.8 MB, less than 65.92% of C++ online submissions for Group Anagrams.
+*/
+
+// Use unordered_map
+/*
+Runtime: 28 ms, faster than 93.71% of C++ online submissions for Group Anagrams.
+Memory Usage: 20.7 MB, less than 48.27% of C++ online submissions for Group Anagrams.
 */
