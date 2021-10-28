ae3b4aed5e80d57f5b7f7a858e7132c53be68320
leopardshadow <thurtu811@gmail.com>
2021-10-28 22:05:04 +0800

    add 977. Squares of a Sorted Array

---
+++ e977.cpp
@@ -0,0 +1,36 @@
+/*
+2 pointer
+一開始我的想法是先找到 0，再依序從那個往左往右找，但題目不一定會出現 0
+可以一樣從頭尾找，加入答案的 vector 時放在最後面 ()
+*/
+class Solution {
+public:
+    static int square(int x) {
+        return x * x;
+    }
+    vector<int> sortedSquares(vector<int>& nums) {
+        
+        vector <int> ans;
+
+        int l = 0, r = nums.size() - 1;
+
+        while(l <= r) {
+            int lSq = square(nums[l]),
+                rSq = square(nums[r]);
+            if(lSq >= rSq) {
+                ans.insert(ans.begin(), lSq);
+                l++;
+            }
+            else {
+                ans.insert(ans.begin(), rSq);
+                r--;
+            }
+        }
+        return ans;
+    }
+};
+
+/*
+Runtime: 568 ms, faster than 5.03% of C++ online submissions for Squares of a Sorted Array.
+Memory Usage: 26.9 MB, less than 27.38% of C++ online submissions for Squares of a Sorted Array.
+*/
