f753c6143e8c79272e8a813472448ce940393439
leopardshadow <thurtu811@gmail.com>
2021-10-31 15:30:43 +0800

    update 295. Find Median from Data Stream

--- h295.cpp
+++ h295.cpp
@@ -22,26 +22,18 @@ public:
         int tmp;
         // should add to maxHeap
         if(maxHeap.size() < minHeap.size()) {
-            
-            if(num > minHeap.top()) {
-                maxHeap.push(minHeap.top());
-                minHeap.pop();
-                minHeap.push(num);
-            }
-            else {
-                maxHeap.push(num);
-            }
+            // 這裡這樣寫比較精簡，之前一堆比較的...
+            // 想法是現在要加到 maxHeap 去，但又要維持 minHeap 元素都比 maxHeap 大
+            // 所以先丟到 minHeap 去，再把 minHeap 最小的 pop 出來丟到 maxHeap
+            minHeap.push(num);
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
         }
         // should add to minHeap
         else {
-            if(num > maxHeap.top()) {
-                minHeap.push(num);
-            }
-            else {
-                minHeap.push(maxHeap.top());
-                maxHeap.pop();
-                maxHeap.push(num);
-            }
+            maxHeap.push(num);
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
         }
     }
     
@@ -63,5 +55,58 @@ public:
 
 /*
 Runtime: 280 ms, faster than 95.01% of C++ online submissions for Find Median from Data Stream.
+Memory Usage: 116.9 MB, less than 90.42% of C++ online submissions for Find Median from Data Stream.
+*/
+
+///////////////////////////////////////
+
+
+class MedianFinder {
+public:
+    
+    // size of minHeap is always equal or one bigger than maxHeap
+    priority_queue <int, vector<int>, greater<int>> minHeap;
+    priority_queue <int, vector<int>> maxHeap;
+    
+    MedianFinder() {
+        
+    }
+    
+    void addNum(int num) {
+        
+        if(minHeap.size() == 0) {
+            minHeap.push(num);
+            return;
+        }
+        
+        int tmp;
+        // should add to maxHeap
+        if(maxHeap.size() < minHeap.size()) {
+            // 這裡這樣寫比較精簡，之前一堆比較的...
+            // 想法是現在要加到 maxHeap 去，但又要維持 minHeap 元素都比 maxHeap 大
+            // 所以先丟到 minHeap 去，再把 minHeap 最小的 pop 出來丟到 maxHeap
+            // 不過這樣時間比較久一點，畢竟是讓 heap 在那邊幫我們慢慢做事
+            minHeap.push(num);
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+        // should add to minHeap
+        else {
+            maxHeap.push(num);
+            minHeap.push(maxHeap.top());
+            maxHeap.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(maxHeap.size() == minHeap.size())
+            return (maxHeap.top() + minHeap.top()) / 2.0;
+        else
+            return minHeap.top();
+    }
+};
+
+/*
+Runtime: 304 ms, faster than 73.62% of C++ online submissions for Find Median from Data Stream.
 Memory Usage: 116.9 MB, less than 90.42% of C++ online submissions for Find Median from Data Stream.
 */
