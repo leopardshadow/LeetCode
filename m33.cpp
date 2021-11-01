1b400f65557bb8503c65307c6dc8b9a6bb424278
leopardshadow <thurtu811@gmail.com>
2021-11-01 15:17:30 +0800

    update 33. Search in Rotated Sorted Array 100% time !!

--- m33.cpp
+++ m33.cpp
@@ -1,3 +1,5 @@
+// D&C
+
 class Solution {
 public:
     int search(vector<int>& nums, int target, int l, int r) {
@@ -6,12 +8,13 @@ public:
             if(nums[l] == target) return l;
             else return -1;
         }
-        // it is sorted
+        // it is sorted and target out of range
         else if(nums[l] < nums[r] &&
                 (target < nums[l] || target > nums[r]))
             return -1;
 
         int m = (l + r) / 2;
+        if(target == nums[m]) return m;  // 加速!! 不然他一定要等到 l == r 才能回傳找到的
         return max(search(nums, target, l, m), search(nums, target, m+1, r));
     }
 
@@ -21,6 +24,6 @@ public:
 };
 
 /*
-Runtime: 8 ms, faster than 23.33% of C++ online submissions for Search in Rotated Sorted Array.
-Memory Usage: 11.1 MB, less than 75.05% of C++ online submissions for Search in Rotated Sorted Array.
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Search in Rotated Sorted Array.
+Memory Usage: 11.1 MB, less than 29.52% of C++ online submissions for Search in Rotated Sorted Array.
 */
