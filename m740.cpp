a329e7c0ba3a3f6594b3ae88a3d65af21cc9ffdf
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-11-18 14:41:13 +0800

    Update m740.cpp

--- m740.cpp
+++ m740.cpp
@@ -7,6 +7,7 @@ sum(n) - 所有數字 n 的和
 ---
 從頭到尾沒看答案生出來的結果，速度超慢，只贏 8.04%
 優化了一點點，現在到 10.06%，主要應該是差在往前跑那裡
+最後參考了別人的解答，才弄到 93.64%
 */
 
 class Record {
@@ -15,7 +16,6 @@ public:
     Record(int n) {
         this->n = n;
         sum = n;
-        maxV = 0;
     }
 };
 
@@ -32,17 +32,19 @@ public:
             else
                 table.push_back(Record(nums[i]));
         }
-        table[0].maxV = table[0].sum;
+        int dp1, dp2;
+        
+        dp1 = table[0].sum;
         
         if(table.size() == 1)
             return table[0].sum;
         
         if(table[1].n == table[0].n + 1)
-            table[1].maxV = table[1].sum;     
+            dp2 = table[1].sum;     
         else 
-            table[1].maxV = table[0].maxV + table[1].sum;
+            dp2 = dp1 + table[1].sum;
             
-        ans = max(table[0].maxV, table[1].maxV);
+        ans = max(dp1, dp2);
         
         for(int i = 2; i < table.size(); i++) {
             
@@ -50,18 +52,14 @@ public:
             // if the previos is a neighbor number
             // we'll need to check. the previos one
             if(table[i].n == table[i-1].n + 1) {
-                j = i - 2;
-                while(j >= 0) {
-                    table[i].maxV = max(table[i].maxV, table[j].maxV + table[i].sum);
-                    j--;
-                }
-                ans = max(ans, table[i].maxV);
+                ans = max(dp2, dp1 + table[i].sum);
             }
             // otherwise, we can directly add 
             else {
-                table[i].maxV = ans + table[i].sum;
-                ans = table[i].maxV;
+                ans = ans + table[i].sum;
             }
+            dp1 = max(dp1, dp2);
+            dp2 = ans;
         }
         return ans;
     }
@@ -69,6 +67,6 @@ public:
 
 
 /*
-Runtime: 24 ms, faster than 10.06% of C++ online submissions for Delete and Earn.
-Memory Usage: 9.4 MB, less than 71.46% of C++ online submissions for Delete and Earn.
+Runtime: 4 ms, faster than 93.64% of C++ online submissions for Delete and Earn.
+Memory Usage: 9.5 MB, less than 71.46% of C++ online submissions for Delete and Earn.
 */
