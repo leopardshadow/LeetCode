6078197284c51eeb62518ecc3f54da1e41e1a7fe
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-11-18 16:14:59 +0800

    Update e53.cpp

--- e53.cpp
+++ e53.cpp
@@ -60,3 +60,34 @@ public:
 Runtime: 96 ms, faster than 55.40% of C++ online submissions for Maximum Subarray.
 Memory Usage: 67.7 MB, less than 53.82% of C++ online submissions for Maximum Subarray.
 */
+
+
+/*
+這種題目有一些變形，像這題是**至少要取一個**，所以答案可能是負的
+如果是可以不取，寫起來又會有點不同
+感覺也可以用 sliding window 的感覺寫
+好怕 DP 的題目 QQ
+*/
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans = nums[0], curSum = nums[0];
+        for(int i = 1; i < nums.size(); i++) {
+            curSum = curSum + nums[i];
+            if(curSum >= 0) {
+                curSum = max(nums[i], curSum);
+                ans = max(ans, curSum);
+            }
+            else {
+                curSum = nums[i];
+                ans = max(ans, nums[i]);
+            }
+        }
+        return ans;
+    }
+};
+
+/*Runtime: 104 ms, faster than 62.46% of C++ online submissions for Maximum Subarray.
+Memory Usage: 67.8 MB, less than 11.26% of C++ online submissions for Maximum Subarray.
+*/
