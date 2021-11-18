5770f012d60d68163a9f7ba30df8345bbf376ed2
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-11-18 23:39:57 +0800

    Update m395.cpp

--- m395.cpp
+++ m395.cpp
@@ -3,13 +3,15 @@
 感覺可以用 divide & conquer 解
 ---
 不看解答寫的，看一下討論區的 tag，果然主要是這兩種
-(寫的時候一樓有人一直大聲講電話，估計整棟都聽得到，煩死)
+(寫的時候一樓有人一直大聲講電話，估計整棟都聽得到，煩死，怎麼可以這麼吵)
 ---
 我的想法是這樣的..
 當我把整串字串跑過一遍後，我會知道各個字元出現的的頻率。
 如果該字元有出現過但次數不滿 k，那些字元就形成了界線，我在取 substring 時一定不能包含他們。
 原本的字元被那些出現不滿 k 次的切開成很多 substring，這些 substring 再往下 recursivly 得做，
-直到那個字串從頭到尾出現字元的次數 >= k
+直到那個字串從頭到尾出現字元的次數都 >= k
+---
+看到討論區類似的寫法不是像我一樣用 l, r 表示頭尾，而是用 substr，看起來比較簡潔
 */
 
 class Solution {
@@ -21,7 +23,7 @@ public:
         return ans;
     }
     
-    void solve(string s, int k, int l, int r) {
+    void solve(string &s, int &k, int l, int r) {
         if(l > r) return;
         if(r - l + 1 <= ans) return;
         
@@ -52,6 +54,14 @@ public:
         solve(s, k, prevJ, j-1);
     }
 };
+
+
+// 把 void solve(string &s, int &k, int l, int r) 的 s 和 k 改成 call by reference，performance 瞬間飆上去，酷ㄟ
+// 話說為了確認他沒有騙我，我再點一次 submit，使用記憶體仍然是 6.3 MB 但變成 94.81% ????? 
+/*
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Longest Substring with At Least K Repeating Characters.
+Memory Usage: 6.3 MB, less than 82.09% of C++ online submissions for Longest Substring with At Least K Repeating Characters.
+*/
 
 /*
 Runtime: 8 ms, faster than 49.18% of C++ online submissions for Longest Substring with At Least K Repeating Characters.
