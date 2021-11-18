6e3f4f9d9f3dfdf98fd3249e19d0f82e14fce5af
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-11-18 12:13:44 +0800

    add 509. Fibonacci Number

---
+++ e509.cpp
@@ -0,0 +1,24 @@
+// DP Day 1
+
+/*
+0 1 2 3 5 8 13 
+a b
+*/
+class Solution {
+public:
+    int fib(int n) {
+        int a = 0, b = 1, tmp;
+        while(n) {
+            tmp = b;
+            b = a + b;
+            a = tmp;
+            n--;
+        }
+        return a;
+    }
+};
+
+/*
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Fibonacci Number.
+Memory Usage: 5.8 MB, less than 79.40% of C++ online submissions for Fibonacci Number.
+*/
