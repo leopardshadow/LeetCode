9899e226c85b1dfbc6c443123582b7b942a89918
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-11-20 20:04:05 +0800

    740. Delete and Earn

--- m740.cpp
+++ m740.cpp
@@ -70,3 +70,61 @@ public:
 Runtime: 4 ms, faster than 93.64% of C++ online submissions for Delete and Earn.
 Memory Usage: 9.5 MB, less than 71.46% of C++ online submissions for Delete and Earn.
 */
+
+// DP D3
+// 基本上 DP 的題目寫完狀態轉移的式子差不多就結束惹
+/*
+這題大致的想法是
+我先把出現的數字和他造成的和統計一下，依 key value 排序
+接著，對於 i 位置，如果
+(1) 他是前一個數字加一，則我們不能都取，所以選 往前兩個最大的 + i 位的和 和 往前一個最大的，兩者間取 max
+(2) 如果不是，那就代表這個數字和前一格數字可以都取就加起來
+(1) / (2) 對應的就是迴圈中的 if / else 
+*/ 
+/*** 下面這個寫法應該比較符合我的直觀w ***/
+
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        unordered_map <int, int> ma;
+        for(int n : nums)
+            ma[n] += n;
+        
+        vector< pair<int, int> > m;
+        
+        for(unordered_map <int, int>::iterator it = ma.begin(); it != ma.end(); it++)
+            m.push_back({it->first, it->second});
+        sort(m.begin(), m.end());    
+        
+        int dp2 = m[0].second,
+            dp1,
+            dp;
+
+        if(m.size() == 1)
+            return m[0].second;
+        
+        if(m[0].first + 1 == m[1].first)
+            dp1 = max(m[0].second, m[1].second);
+        else
+            dp1 = m[0].second + m[1].second;
+        
+        if(m.size() == 2) {
+            return dp1;
+        }
+        for(int i = 2; i < m.size(); i++) {
+            if(m[i-1].first + 1 == m[i].first)
+                dp = max(dp1, 
+                         dp2 + m[i].second);
+            else
+                dp = dp1 + m[i].second;
+            dp2 = dp1;
+            dp1 = dp;
+        }
+        return dp;
+    }
+};
+
+/*
+Runtime: 4 ms, faster than 93.60% of C++ online submissions for Delete and Earn.
+Memory Usage: 10 MB, less than 55.19% of C++ online submissions for Delete and Earn.
+*/
