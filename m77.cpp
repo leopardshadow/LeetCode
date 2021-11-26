35b1f0c0cc2abd09a7323bfb2e296b66b73f72d0
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-11-26 14:44:09 +0800

    77. Combinations

--- m77.cpp
+++ m77.cpp
@@ -1,3 +1,5 @@
+// backtracking
+
 class Solution {
     vector<vector<int>> ans;
     vector<int> c;
@@ -20,4 +22,31 @@ public:
 /*
 Runtime: 34 ms, faster than 40.41% of C++ online submissions for Combinations.
 Memory Usage: 10.1 MB, less than 40.97% of C++ online submissions for Combinations.
+*/
+
+//***************************************************************************//
+class Solution {
+    vector<vector<int>> ans;
+    vector<int> c;
+public:
+    void comb(int start, int n, int k) {
+        if (k <= 0) {
+            ans.push_back(c);
+            return;   // 重要 ...
+        }
+        for(int i = start; i <= n - k + 1; i++) {
+            c.push_back(i);
+            comb(i + 1, n, k - 1);
+            c.pop_back();
+        }
+    }
+    vector<vector<int>> combine(int n, int k) {
+        comb(1, n, k);
+        return ans;
+    }
+};
+
+/*
+Runtime: 8 ms, faster than 96.98% of C++ online submissions for Combinations.
+Memory Usage: 10.1 MB, less than 47.54% of C++ online submissions for Combinations.
 */
