97cdecd85cd45c9686b32b8ae3ca57a73f6a0483
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-11-26 22:54:42 +0800

    78. Subsets

--- m78.cpp
+++ m78.cpp
@@ -38,4 +38,42 @@ public:
 /*
 Runtime: 7 ms, faster than 16.49% of C++ online submissions for Subsets.
 Memory Usage: 15.8 MB, less than 15.60% of C++ online submissions for Subsets.
+
+
+
+//***************************************************************************//
+/*
+之前寫過一個作法是 to take or not to take，用 recursive 產生所有可能，今天來換一種寫法
+拿或不拿，很自然就會想到 binary，用二進位數字的 1/0 表示取/不取
+000
+001
+010
+011
+100
+101
+110
+111
+*/
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        for (int ss = 0; ss < (1 << n); ss++) {
+            vector<int> subset;
+            for (int i = 0; i < n; i++) {
+                if (ss & (1 << i))
+                    subset.push_back(nums[i]);
+            }
+            ans.push_back(subset);
+        }
+        return ans;
+    }
+};
+
+/*
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Subsets.
+Memory Usage: 7 MB, less than 83.01% of C++ online submissions for Subsets.
+*/
 */
