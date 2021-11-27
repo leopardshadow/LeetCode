be651b2d929a17bcd1a666e6502ca4e76adb6655
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-11-27 16:28:55 +0800

    91. Decode Ways

--- m91.cpp
+++ m91.cpp
@@ -1,3 +1,8 @@
+/*
+原來我之前是用 map 喔 XDD
+下面那個寫法應該比較直覺~~
+*/
+
 
 /*
 s0 s1 s2 s3 s4
@@ -37,4 +42,37 @@ public:
 /*
 Runtime: 4 ms, faster than 31.70% of C++ online submissions for Decode Ways.
 Memory Usage: 8.1 MB, less than 8.11% of C++ online submissions for Decode Ways.
+*/
+
+//***************************************************************************//
+// DP D10
+/*
+table[i] stroes the number of possible ways of splitting for the string s[0:i]
+
+table[i] = table[i-1] (if string[i] is valid, otherwise: 0) +
+           table[i-2] (if string.substr(i-1, 2) is valid, otherwise: 0)
+
+*/
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        if (s[0] == '0')
+            return 0;
+        if (s.length() == 1)
+            return 1;
+        vector<int> table(s.length());
+        table[0] = 1;
+        table[1] = (s[1] == '0' ? 0 : table[0]) + (stoi(s.substr(0, 2)) <= 26 ? 1 : 0);
+        for (int i = 2; i < s.length(); i++) {
+            table[i] = (s[i] == '0' ? 0 : table[i-1]) + 
+                        (stoi(s.substr(i-1, 2)) <= 26 && s[i-1] != '0' ? table[i-2] : 0);
+        }
+        return table.back();
+    }
+};
+
+/*
+Runtime: 7 ms, faster than 11.67% of C++ online submissions for Decode Ways.
+Memory Usage: 6.1 MB, less than 91.18% of C++ online submissions for Decode Ways.
 */
