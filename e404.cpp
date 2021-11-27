249d25b6cceef51d05bdc0f0d3288cd2f90d1a18
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-11-27 11:12:14 +0800

    404. Sum of Left Leaves

--- e404.cpp
+++ e404.cpp
@@ -46,3 +46,37 @@ public:
 Runtime: 0 ms, faster than 100.00% of C++ online submissions for Sum of Left Leaves.
 Memory Usage: 13.5 MB, less than 11.34% of C++ online submissions for Sum of Left Leaves.
 */
+
+
+
+/*
+練習用 BFS，迭代寫法
+*/
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if(!root)
+            return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int sum = 0;
+        while(!q.empty()) {
+            TreeNode *n = q.front();
+            q.pop();
+            //----------------------------------------------
+            if(n->left && !n->left->right && !n->left->left)
+                sum += n->left->val;
+            else if(n->left)
+                q.push(n->left);
+            //----------------------------------------------
+            if(n->right)
+                q.push(n->right);
+        }
+        return sum;
+    }
+};
+
+/*
+Runtime: 8 ms, faster than 25.12% of C++ online submissions for Sum of Left Leaves.
+Memory Usage: 13.6 MB, less than 8.89% of C++ online submissions for Sum of Left Leaves.
+*/
