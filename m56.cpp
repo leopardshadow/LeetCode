8a89fc3526aae97615a6428018170e81d06ed66f
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-11-28 22:21:57 +0800

    56. Merge Intervals

--- m56.cpp
+++ m56.cpp
@@ -37,3 +37,32 @@ public:
 Runtime: 30 ms, faster than 33.43% of C++ online submissions for Merge Intervals.
 Memory Usage: 14.3 MB, less than 56.68% of C++ online submissions for Merge Intervals.
 */
+
+
+//***************************************************************************//
+// Review: 2021.11.28 before G VO
+
+/*
+嘗試了 lambda function 比較大小 XD
+*/
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // sort by its start time
+        sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int> b){
+            return a[0] < b[0];
+        });
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+        for(int i = 1; i < intervals.size(); i++) {
+            // merge!
+            if(intervals[i][0] <= ans.back()[1])
+                ans.back()[1] = max(intervals[i][1], ans.back()[1]);
+            // disjoint
+            else
+                ans.push_back(intervals[i]);
+        }
+        return ans;
+    }
+};
