dfee216303c30b211b2bc79c415af6939e5683b8
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-11-28 15:49:35 +0800

    96. Unique Binary Search Trees

--- m96.cpp
+++ m96.cpp
@@ -45,3 +45,27 @@ public:
 Runtime: 0 ms, faster than 100.00% of C++ online submissions for Unique Binary Search Trees.
 Memory Usage: 6 MB, less than 37.05% of C++ online submissions for Unique Binary Search Trees.
 */
+
+
+//***************************************************************************//
+// DP D11
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> table(n + 1, 0);
+        table[0] = 1;
+        table[1] = 1;
+        for(int i = 2; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                table[i] += table[j] * table[i-j-1];
+            }
+        }
+        return table.back();
+    }
+};
+
+/*
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Unique Binary Search Trees.
+Memory Usage: 6.1 MB, less than 34.26% of C++ online submissions for Unique Binary Search Trees.
+*/
