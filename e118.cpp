9954f50b2af3380bf8dbc04aada611c4c5a9a747
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-11-29 20:33:52 +0800

    118. Pascal's Triangle

---
+++ e118.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> ans(numRows);
+        ans[0].push_back(1);
+        for(int layer = 1; layer < numRows; layer++) {
+            ans[layer].push_back(1);
+            for(int j = 1; j < ans[layer-1].size(); j++) {
+                ans[layer].push_back(ans[layer-1][j-1] + ans[layer-1][j]);
+            }
+            ans[layer].push_back(1);
+        }
+        return ans;
+    }
+};
+
+/*
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Pascal's Triangle.
+Memory Usage: 6.6 MB, less than 33.35% of C++ online submissions for Pascal's Triangle.
+*/
