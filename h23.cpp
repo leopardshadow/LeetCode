352a8f0c2f5eba44e7564f47244fda88518f5e21
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-12-02 09:49:48 +0800

    23. Merge k Sorted Lists

--- h23.cpp
+++ h23.cpp
@@ -1,41 +1,41 @@
 //***************************************************************************//
-// 2021.12.2 before G VO (聽說一關炸彈其他關好還是可能過~)
+// 2021.12.2 before G VO (聽說一關炸掉，但其他關好還是可能過~)
 
 class Solution {
     
     struct comp {
-        bool operator() (const pair<int, ListNode*> &p1, const pair<int, ListNode*> &p2) {
-            return p1.first > p2.first;
+        bool operator() (const ListNode *p1, const ListNode *p2) {
+            return p1->val > p2->val;
         }
     };
     
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         
-        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, comp> pq;
+        priority_queue<ListNode*, vector<ListNode*>, comp> pq;
         
         for(int i = 0; i < lists.size(); i++) {
             if(lists[i])
-                pq.push({lists[i]->val, lists[i]});
+                pq.push(lists[i]);
         }
         
         ListNode *root, *prev = NULL;
         if(pq.empty())
             root = NULL;
         else {
-            root = pq.top().second;
+            root = pq.top();
             pq.pop();
             if(root->next)
-                pq.push({root->next->val, root->next});
+                pq.push(root->next);
             prev = root;
         }
              
         while(!pq.empty()) {
             
-            ListNode *cur = pq.top().second;
+            ListNode *cur = pq.top();
             pq.pop();
             if(cur->next)
-                pq.push({cur->next->val, cur->next});
+                pq.push(cur->next);
             
             prev->next = cur;
             
@@ -46,7 +46,14 @@ public:
     }
 };
 
+// 用 pair
 /*
 Runtime: 16 ms, faster than 97.61% of C++ online submissions for Merge k Sorted Lists.
 Memory Usage: 13.6 MB, less than 39.66% of C++ online submissions for Merge k Sorted Lists.
+*/
+
+// 只用 ListNode *
+/*
+Runtime: 20 ms, faster than 88.09% of C++ online submissions for Merge k Sorted Lists.
+Memory Usage: 13.4 MB, less than 59.20% of C++ online submissions for Merge k Sorted Lists.
 */
