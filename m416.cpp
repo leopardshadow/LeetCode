732f4166f90d6d825a36c199a1638ed3deba733a
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-12-12 19:00:49 +0800

    416. Partition Equal Subset Sum

--- m416.cpp
+++ m416.cpp
@@ -35,3 +35,39 @@ public:
 Runtime: 200 ms, faster than 46.67% of C++ online submissions for Partition Equal Subset Sum.
 Memory Usage: 9.9 MB, less than 81.36% of C++ online submissions for Partition Equal Subset Sum.
 */
+
+
+
+
+// 2021.12.12
+
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        
+        int sum = 0;
+        for(int n : nums)
+            sum += n;
+        
+        // odd
+        if(sum % 2)
+            return false;
+        
+        int halfSum = sum / 2;
+        
+        vector<int> dp(halfSum + 1, 0);
+        
+        for(int i = 0; i < nums.size(); i++) {
+            for(int s = halfSum; s >= 0; s--) {
+                if(s - nums[i] >= 0)
+                    dp[s] = max(dp[s], nums[i] + dp[s - nums[i]]);
+            }
+        }        
+        return dp.back() == halfSum;
+    }
+};
+
+/*
+Runtime: 208 ms, faster than 43.15% of C++ online submissions for Partition Equal Subset Sum.
+Memory Usage: 9.9 MB, less than 77.84% of C++ online submissions for Partition Equal Subset Sum.
+*/
