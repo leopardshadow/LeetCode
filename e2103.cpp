5390981ad2a69c74e856166339324593e3ac18a0
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-12-12 12:07:42 +0800

    2103. Rings and Rods

---
+++ e2103.cpp
@@ -0,0 +1,27 @@
+// Weekly Contest 271
+
+class Solution {
+public:
+    int countPoints(string rings) {
+        
+        int rods[10] = {0};
+        
+        for(int i = 0; i < rings.length() / 2; i++) {
+            
+            int n = rings[2 * i + 1] - '0';
+            
+            if(rings[2 * i] == 'R')
+                rods[n] |= 1;
+            else if(rings[2 * i] == 'G')
+                rods[n] |= 2;
+            else
+                rods[n] |= 4;
+        }
+        int ans = 0;
+        for(int i = 0; i < 10; i++) {
+            if(rods[i] == 7)
+                ans++;
+        }
+        return ans;
+    }
+};
