eb37c72ed6382eb7b8c4e1b3be9dac20094f7d36
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-12-12 12:08:33 +0800

    2104. Sum of Subarray Ranges

---
+++ m2104.cpp
@@ -0,0 +1,21 @@
+/*
+居然 data type 是 long long，好少遇到喔
+---
+sliding window
+*/
+
+class Solution {
+public:
+    long long subArrayRanges(vector<int>& nums) {
+        long long ans = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            long long nmax = nums[i], nmin = nums[i];
+            for(int j = i + 1; j < nums.size(); j++) {
+                nmax = max(nmax, (long long)nums[j]);
+                nmin = min(nmin, (long long)nums[j]);
+                ans = ans + nmax - nmin;
+            }
+        }
+        return ans;
+    }
+};
