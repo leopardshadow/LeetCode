1985a426f35b437be03cda2ed36b7e4e66927532
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-12-14 23:56:11 +0800

    2092. Find All People With Secret

--- h2092.cpp
+++ h2092.cpp
@@ -99,42 +99,72 @@ public:
     }
 };
 
-
+//******************************************************************************//
 /*
 寫了簡單版本的 union find，但還是 TLE QQ
 看來要加上 map 了
 */
 
+/*
+disjoint set 優化：
+
+1. path compression
+2. union by rank
+---
+(感覺在面試寫出有 path compression 的就好w，rank 好容易錯的感覺)
+---
+但即使這樣，還是會 TLE，我讓 reset 那邊不要全部跑過，而是用一個 vector toReset 紀錄才跑出 AC
+
+這裡有個有趣的問題
+---
+*/
+
 class UnionFind {
     vector<int> nodes;
+    vector<int> ranks;
+    vector<int> toReset;
     int n;
 public:
-    UnionFind(int n) : nodes(n) {
+    UnionFind(int n) : nodes(n), ranks(n, 1) {
         this->n = n;
         for(int i = 0; i < n; i++) {
             nodes[i] = i;
         }
     }
-    int getRoot(int i) {
-        return nodes[i];
+    int find(int x) {
+        if(x == nodes[x])
+            return x;
+        return nodes[x] = find(nodes[x]);
     }
     // change x's value to y's
     void connect(int x, int y) {
-        int rootX = nodes[x],
-            rootY = nodes[y];
-        for(int i = 0; i < n; i++) {
-            if(nodes[i] == rootY)
-                nodes[i] = rootX;
-        }
+        int rootX = find(x),
+            rootY = find(y);
+        nodes[rootX] = rootY;
+//         if(rootX != rootY) {
+//             if (ranks[rootX] > ranks[rootY]) {
+//                 nodes[rootY] = rootX;
+//             } 
+//             else if(ranks[rootX] < ranks[rootY]) {
+//                 nodes[rootX] = rootY;
+//             } 
+//             else {
+//                 nodes[rootY] = rootX;
+//                 ranks[rootX] += 1;
+//             }
+//         }
+        toReset.push_back(x);
+        toReset.push_back(y);
     }
     bool isConnected(int x, int y) {
-        return nodes[x] == nodes[y];
+        return find(x) == find(y);
     }
     void reset() {
-        for(int i = 0; i < n; i++) {
+        for(int i : toReset) {
             if(!isConnected(0, i))
                 nodes[i] = i;
         }
+        toReset.clear();
     }
 };
 
@@ -157,7 +187,6 @@ public:
             if(meetings[i][2] != meetings[i-1][2]) {
                 uf.reset();
             }
-            
             uf.connect(meetings[i][0], meetings[i][1]);
         }
         
@@ -169,3 +198,95 @@ public:
         return ans;
     }
 };
+
+
+/*
+Runtime: 746 ms, faster than 52.72% of C++ online submissions for Find All People With Secret.
+Memory Usage: 138.9 MB, less than 84.88% of C++ online submissions for Find All People With Secret.
+*/
+
+//******************************************************************************//
+/*
+拿掉 rank 反而更快?????
+Leetcode 的執行時間真的很謎
+*/
+
+class UnionFind {
+    vector<int> nodes;
+    vector<int> toReset;
+    int n;
+public:
+    UnionFind(int n) : nodes(n) {
+        this->n = n;
+        for(int i = 0; i < n; i++) {
+            nodes[i] = i;
+        }
+    }
+    int find(int x) {
+        if(x == nodes[x])
+            return x;
+        return nodes[x] = find(nodes[x]);
+    }
+    // change x's value to y's
+    void connect(int x, int y) {
+        int rootX = find(x),
+            rootY = find(y);
+        nodes[rootX] = rootY;
+
+        toReset.push_back(x);
+        toReset.push_back(y);
+    }
+    bool isConnected(int x, int y) {
+        return find(x) == find(y);
+    }
+    void reset() {
+        for(int i : toReset) {
+            if(!isConnected(0, i))
+                nodes[i] = i;
+        }
+        toReset.clear();
+    }
+};
+
+class Solution {
+public:
+    vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
+        
+        // sort the meetings by their time
+        sort(meetings.begin(), meetings.end(), [](vector<int> &a, vector<int> &b){
+            return a[2] < b[2];
+        });
+        
+        UnionFind uf(n);
+        
+        uf.connect(0, firstPerson);
+        uf.connect(meetings[0][0], meetings[0][1]);
+        
+        for(int i = 1; i < meetings.size(); i++) {
+                        
+            if(meetings[i][2] != meetings[i-1][2]) {
+                uf.reset();
+            }
+            uf.connect(meetings[i][0], meetings[i][1]);
+        }
+        
+        vector<int> ans;
+        for(int i = 0; i < n; i++) {
+            if(uf.isConnected(0, i))
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
+
+// 一樣的 code，差超多的執行時間
+
+/*
+Runtime: 592 ms, faster than 82.15% of C++ online submissions for Find All People With Secret.
+Memory Usage: 142 MB, less than 82.39% of C++ online submissions for Find All People With Secret.
+*/
+
+/*
+Runtime: 872 ms, faster than 42.22% of C++ online submissions for Find All People With Secret.
+Memory Usage: 141.9 MB, less than 82.58% of C++ online submissions for Find All People With Secret.
+*/
