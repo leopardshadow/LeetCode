76f4435f31dac97839d4ed2bc82d3c582dc21d43
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-12-22 23:04:47 +0800

    143. Reorder List

--- m143.cpp
+++ m143.cpp
@@ -71,3 +71,47 @@ public:
 Runtime: 36 ms, faster than 88.05% of C++ online submissions for Reorder List.
 Memory Usage: 17.7 MB, less than 76.61% of C++ online submissions for Reorder List.
 */
+
+
+// 2021.12.22
+
+/*
+這題題目有點小複雜，要好好理解到底要求怎麼連
+---
+之前的寫法是連來連去的，這次用比較偷懶 (?) 的寫法
+額外用了 O(n) 的 space
+---
+毫無疑問得用了比較多 memory，但速度加快好多
+*/
+
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+     
+        vector<ListNode*> arr;
+        
+        ListNode* cur = head->next;
+        while(cur) {
+            arr.push_back(cur);
+            cur = cur->next;
+        }
+        int n = arr.size(), i, j;
+        cur = head;
+        for(i = 0, j = n - 1; i < j; i++, j--) {
+            cur->next = arr[j];
+            cur = cur->next;
+            cur->next = arr[i];
+            cur = cur->next;
+        }
+        if(i == j) {
+            cur->next = arr[i];
+            cur = cur->next;
+        }
+        cur->next = NULL;
+    }
+};
+
+/*
+Runtime: 32 ms, faster than 95.77% of C++ online submissions for Reorder List.
+Memory Usage: 18.9 MB, less than 14.63% of C++ online submissions for Reorder List.
+*/
