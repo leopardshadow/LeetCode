ae79419f5d74cb93a8cd5b39bb1ff1485aeb8370
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-12-23 23:03:09 +0800

    210. Course Schedule II

--- m210.cpp
+++ m210.cpp
@@ -53,3 +53,59 @@ public:
 Runtime: 12 ms, faster than 99.40% of C++ online submissions for Course Schedule II.
 Memory Usage: 13.2 MB, less than 94.85% of C++ online submissions for Course Schedule II.
 */
+
+
+
+
+
+
+// 2021.12.23
+
+/*
+感覺最近的 daily 題好常遇到做過的喔
+---
+就 topologocal sort
+*/
+
+
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    
+        vector<pair<int, vector<int>>> cs(numCourses);
+        
+        // p[1] --> p[0]
+        for(auto p : prerequisites) {
+            cs[p[0]].first++;
+            cs[p[1]].second.push_back(p[0]);
+        }
+        
+        vector<int> ans;
+        queue<int> q;
+        for(int i = 0; i < numCourses; i++) {
+            if(cs[i].first == 0)
+                q.push(i);
+        }
+        while(!q.empty()) {
+            
+            int take = q.front();
+            q.pop();
+            ans.push_back(take);
+            
+            for(auto next : cs[take].second) {
+                cs[next].first--;
+                if(cs[next].first == 0)
+                    q.push(next);
+            }
+        }
+        // 題目要求如果無法修完所有課 (有 circle)，就回傳空的
+        if(ans.size() < numCourses)
+            return {};
+        return ans;
+    }
+};
+
+/*
+Runtime: 24 ms, faster than 47.25% of C++ online submissions for Course Schedule II.
+Memory Usage: 14 MB, less than 61.22% of C++ online submissions for Course Schedule II.
+*/
