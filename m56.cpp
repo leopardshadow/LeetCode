841da7da76639485db29aa1c183e80ee0ef06a56
Cheng-Cheng Lo <thurtu811@gmail.com>
2021-12-24 23:58:55 +0800

    56. Merge Intervals

--- m56.cpp
+++ m56.cpp
@@ -33,6 +33,47 @@ public:
     }
 };
 
+
+
+
+//******************************************************************************//
+// 2021.12.24 平安夜~~
+
+/*
+又是寫過的題目R，最近都只寫每日題，題數沒增加XD
+---
+interval 題目的模式好像都蠻固定的
+---
+一開始我少加 max ...
+*/
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin(), intervals.end(), [](vector<int> &a, vector<int> &b){
+            return a[0] < b[0];
+        });
+        
+        vector<vector<int>> ans;
+        
+        ans.push_back(intervals[0]);
+        
+        for(int i = 1; i < intervals.size(); i++) {
+            if(intervals[i][0] <= ans.back()[1])
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            else
+                ans.push_back(intervals[i]);
+        }
+        return ans;
+    }
+};
+
+/*
+Runtime: 20 ms, faster than 62.64% of C++ online submissions for Merge Intervals.
+Memory Usage: 14.1 MB, less than 94.91% of C++ online submissions for Merge Intervals.
+*/
+
 /*
 Runtime: 30 ms, faster than 33.43% of C++ online submissions for Merge Intervals.
 Memory Usage: 14.3 MB, less than 56.68% of C++ online submissions for Merge Intervals.
