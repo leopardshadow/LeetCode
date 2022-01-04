c4ffa7b64ff90282e8120e7c37598a78c52ee60a
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-01-04 23:19:39 +0800

    1509. Minimum Difference Between Largest and Smallest Value in Three Moves

--- m1509.cpp
+++ m1509.cpp
@@ -27,3 +27,53 @@ public:
 Runtime: 80 ms, faster than 92.08% of C++ online submissions for Minimum Difference Between Largest and Smallest Value in Three Moves.
 Memory Usage: 35.3 MB, less than 51.17% of C++ online submissions for Minimum Difference Between Largest and Smallest Value in Three Moves.
 */
+
+
+//******************************************************************************//
+
+/*
+看到這題回來翻了一下，覺得之前的寫法有點花時間，我只需要存最大和最小的 3 個，應該可以不要對整個陣列排序
+所以我分別用兩個大小為 4 的 heap 存，這樣複雜度從 n log n 降成 n log 4
+但實際執行起來跑的時間卻變久 ... 好怪
+*/
+
+class Solution {
+public:
+    int minDifference(vector<int>& nums) {
+        
+        if(nums.size() <= 4)
+            return 0;
+        
+        priority_queue<int> minHeap;
+        priority_queue<int, vector<int>, greater<int>> maxHeap;
+        
+        for(int i = 0; i < 4; i++) {
+            minHeap.push(nums[i]);
+            maxHeap.push(nums[i]);
+        }
+        // keeps the size of heap 4
+        for(int i = 4; i < nums.size(); i++) {
+            minHeap.push(nums[i]);
+            minHeap.pop();
+            maxHeap.push(nums[i]);
+            maxHeap.pop();
+        }
+        int big[4], small[4];
+        for(int i = 0; i < 4; i++) {
+            big[i] = maxHeap.top();
+            maxHeap.pop();
+            small[3 - i] = minHeap.top();
+            minHeap.pop();
+        }
+        int ans = INT_MAX;
+        for(int i = 0; i < 4; i++) {
+            ans = min(ans, big[i] - small[i]);
+        }
+        return ans;
+    }
+};
+
+/*
+Runtime: 239 ms, faster than 5.24% of C++ online submissions for Minimum Difference Between Largest and Smallest Value in Three Moves.
+Memory Usage: 35.3 MB, less than 10.41% of C++ online submissions for Minimum Difference Between Largest and Smallest Value in Three Moves.
+*/
