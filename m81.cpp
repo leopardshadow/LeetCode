2a0296d4d3e9dbe11765b08d6a5f4fbe8f72a0a3
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-01-05 12:18:08 +0800

    81. Search in Rotated Sorted Array II

--- m81.cpp
+++ m81.cpp
@@ -1,19 +1,21 @@
-// class Solution {
-// public:
+// 發現那時候 l, r 好像弄反，和現在習慣不一樣 @@
+
+class Solution {
+public:
     
-//     bool search(vector<int>& nums, int target, int r, int l) {
-//         if(r == l) return nums[r] == target;
-//         int m = (r + l) / 2;
-//         if(target == nums[m]) return true;
-//         if(nums[l] <= nums[r] && target < nums[l] && target > nums[r])
-//             return false;
-//         return search(nums, target, r, m) || search(nums, target, m+1, l);
-//     }
+    bool search(vector<int>& nums, int target, int r, int l) {
+        if(r == l) return nums[r] == target;
+        int m = (r + l) / 2;
+        if(target == nums[m]) return true;
+        if(nums[l] <= nums[r] && target < nums[l] && target > nums[r])
+            return false;
+        return search(nums, target, r, m) || search(nums, target, m+1, l);
+    }
     
-//     bool search(vector<int>& nums, int target) {
-//         return search(nums, target, 0, nums.size()-1);
-//     }
-// };
+    bool search(vector<int>& nums, int target) {
+        return search(nums, target, 0, nums.size()-1);
+    }
+};
 
 /*
 Runtime: 4 ms, faster than 89.33% of C++ online submissions for Search in Rotated Sorted Array II.
