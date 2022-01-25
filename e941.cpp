deed049d781d00abb1737d43006bf3f3246cb11f
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-01-25 10:17:19 +0800

    941. Valid Mountain Array

--- e941.cpp
+++ e941.cpp
@@ -2,12 +2,15 @@
 // 2022.1.25
 
 /*
-這種把陣列走過一次可以知道答案的題目可以想像 FSM 在那邊轉轉轉
----
-但我這題是先找到最高點，從最高點的位置往左走、往右走都要是遞減的
+我這題是先找到最高點，從最高點的位置往左走、往右走都要是遞減的
 這樣的寫法比較簡單，但缺點就是總共走了兩次陣列
+---
+下面也試了只有一次陣列，前後比較大小的版本!
+---
+lee215 的解法好酷，他讓兩個人 i, j 分別從兩端開始往上爬，如果是合法的山他們會在最高點相遇 (+ 檢查 i, j 都有移動)
 */
 
+// 先找到最高點
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
@@ -38,3 +41,31 @@ public:
 Runtime: 46 ms, faster than 27.60% of C++ online submissions for Valid Mountain Array.
 Memory Usage: 22.3 MB, less than 79.54% of C++ online submissions for Valid Mountain Array.
 */
+
+
+// 只有一次陣列，都是前後比較
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        if(arr.size() <= 1)  // == 2 is handled below
+            return false;
+        
+        int i = 1;
+        while(i < arr.size() && arr[i-1] < arr[i])
+            i++;
+        
+        if(i == 1 || i == arr.size())
+            return false;
+        
+        while(i < arr.size() && arr[i-1] > arr[i])
+            i++;
+        
+        return i == arr.size();
+    }
+};
+
+/*
+Runtime: 77 ms, faster than 5.17% of C++ online submissions for Valid Mountain Array.
+Memory Usage: 22.5 MB, less than 48.39% of C++ online submissions for Valid Mountain Array.
+*/
+
