212715d1f9f2493f1f1abb251c425fb8dc6606b5
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-01-30 19:22:03 +0800

    2156. Find Substring With Given Hash Value

---
+++ m2156.cpp
@@ -0,0 +1,27 @@
+
+// 這週 Weekly 的題目，沒參加，但還是寫寫看
+
+/*
+無腦照刻會 TLE QQ
+題目的 hint: rolling hash
+*/
+
+class Solution {
+    int calHash(const string &s, int power, int modulo, int start, int k) {
+        long long p = 1, hashV = 0;
+        for(int i = 0; i < k; i++) {
+            hashV = (hashV + p * (s[start + i] - 'a' + 1)) % modulo;
+            p = (p * power) % modulo;
+        }
+        return hashV % modulo;
+    }
+public:
+    string subStrHash(string s, int power, int modulo, int k, int hashValue) {
+        int n = s.length();
+        for(int i = 0; i + k - 1 < n; i++) {
+            if(calHash(s, power, modulo, i, k) == hashValue)
+                return s.substr(i, k);
+        }
+        return "";  // never returns this according to the constraints
+    }
+};
