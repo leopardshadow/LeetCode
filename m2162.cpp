45205cc56f731c3fb030bcde54129ac06171c0fa
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-02-06 16:19:19 +0800

    2162. Minimum Cost to Set Cooking Time

---
+++ m2162.cpp
@@ -0,0 +1,15 @@
+
+// 這週的 contest，沒參加，但是 ... ww
+
+/*
+G
+*/
+
+class Solution {
+public:
+    int minCostSetTime(int startAt, int moveCost, int pushCost, int targetSeconds) {
+    
+        
+        
+    }
+};
