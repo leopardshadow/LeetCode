370a6ee6da8f7600f6600c2896306ffdd469ca42
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-02-07 18:52:52 +0800

    389. Find the Difference

--- e389.cpp
+++ e389.cpp
@@ -1,6 +1,14 @@
 
 // 2022.2.7 過年後第一天XD
 
+/*
+這題超多種做法的，我一開始是用 map 計算各個字母出現的數量
+---
+也可以計算兩個字串總和的差
+---
+還有 xor!! 100% faster
+*/
+
 class Solution {
 public:
     char findTheDifference(string s, string t) {
@@ -19,4 +27,23 @@ public:
 /*
 Runtime: 8 ms, faster than 17.39% of C++ online submissions for Find the Difference.
 Memory Usage: 6.6 MB, less than 67.00% of C++ online submissions for Find the Difference.
+*/
+
+
+
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        char ans = 0;
+        for(const char &c : t)
+            ans ^= c;
+        for(const char &c : s)
+            ans ^= c;
+        return ans;
+    }
+};
+
+/*
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Find the Difference.
+Memory Usage: 6.7 MB, less than 67.00% of C++ online submissions for Find the Difference.
 */
