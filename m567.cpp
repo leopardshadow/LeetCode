3d289a5cc8a9d21ed25adddde5bfd0943141d43d
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-02-11 09:44:44 +0800

    567. Permutation in String

--- m567.cpp
+++ m567.cpp
@@ -49,3 +49,63 @@ public:
 Runtime: 4 ms, faster than 96.86% of C++ online submissions for Permutation in String.
 Memory Usage: 7.1 MB, less than 98.75% of C++ online submissions for Permutation in String.
 */
+
+
+
+// 2022.2.11
+
+/*
+這題在看題目時花了一點點時間理解
+回傳 true 的條件是 s2 包含 s1 的組合，包含指的是 substring
+題目敘述的第二句只是在補充說明第二句..
+---
+前天以為是 sliding window 的題目結果是 prefix sum
+今天就真的出一個 sliding window
+---
+寫完後看 Submission 才發現原來我之前寫過，居然沒什麼印象 笑死
+複習很重要 +1
+*/
+
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        
+        if(s1.length() > s2.length())
+            return false;
+        
+        int cnt[26] = { 0 };
+        for(int i = 0; i < s1.length(); i++) {
+            cnt[s2[i] - 'a']++;
+            cnt[s1[i] - 'a']--;
+        }
+            
+        bool match = true;
+        for(int i = 0; i < 26; i++) {
+            if(cnt[i] != 0)
+                match = false;
+        }
+        if(match)
+            return true;
+        
+        for(int i = s1.length(); i < s2.length(); i++) {
+            
+            cnt[s2[i - s1.length()] - 'a']--;
+            cnt[s2[i] - 'a']++;
+            
+            match = true;
+            for(int i = 0; i < 26; i++) {
+                if(cnt[i] != 0)
+                    match = false;
+            }
+            if(match)
+                return true;
+        }
+        
+        return false;
+    }
+};
+
+/*
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Permutation in String.
+Memory Usage: 7.3 MB, less than 71.57% of C++ online submissions for Permutation in String.
+*/
