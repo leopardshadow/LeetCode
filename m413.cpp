b70e658a3553f33e160827e79d35d5cd6b61f243
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-03-03 22:17:48 +0800

    413. Arithmetic Slices

--- m413.cpp
+++ m413.cpp
@@ -35,3 +35,39 @@ public:
 Runtime: 3 ms, faster than 62.01% of C++ online submissions for Arithmetic Slices.
 Memory Usage: 7.4 MB, less than 15.64% of C++ online submissions for Arithmetic Slices.
 */
+
+
+
+
+
+// 2022.3.2
+
+/*
+雖然 tag 是 dp，但是稍微列出幾個數字就可以發現規律
+---
+又是一題之前完全沒印象寫過，複習很重要RRR
+*/
+
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        if(nums.size() <= 2)
+            return 0;
+        vector<pair<int, int>> diffs;
+        diffs.push_back({nums[1] - nums[0], 1});
+        for(int i = 2; i < nums.size(); i++) {
+            int d = nums[i] - nums[i-1];
+            if(d == diffs.back().first)
+                diffs.back().second++;
+            else
+                diffs.push_back({d, 1});
+        }
+        int ans = 0;
+        for(const auto &diff : diffs) {
+            int d = diff.second;
+            ans += (1 + d - 1) * (d - 1) / 2;
+        }
+            
+        return ans;
+    }
+};
