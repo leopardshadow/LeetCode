efd9a7062cf865a20aa069824470d6ecad6c092f
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-03-07 09:11:24 +0800

    21. Merge Two Sorted Lists

--- e21.cpp
+++ e21.cpp
@@ -67,3 +67,54 @@ struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
 Runtime: 4 ms, faster than 84.06% of C online submissions for Merge Two Sorted Lists.
 Memory Usage: 6.1 MB, less than 55.13% of C online submissions for Merge Two Sorted Lists.
 */
+
+
+
+
+
+// 2022.3.7
+
+/*
+上一次寫這題是四月面試前，距離現在已經快一年了 OAO
+---
+c -> cpp
+---
+之前寫得偏醜XD
+*/
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *head = new ListNode(), *curr = head;
+        
+        while(list1 && list2) {
+            if(list1->val <= list2->val) {
+                curr->next = list1;
+                curr = curr->next;
+                list1 = list1->next;
+            }
+            else {
+                curr->next = list2;
+                curr = curr->next;
+                list2 = list2->next;
+            }
+        }
+        
+        while(list1) {
+            curr->next = list1;
+            curr = curr->next;
+            list1 = list1->next;
+        }
+        while(list2) {
+            curr->next = list2;
+            curr = curr->next;
+            list2 = list2->next;
+        }
+        
+        return head->next;
+    }
+};
+/*
+Runtime: 3 ms, faster than 97.67% of C++ online submissions for Merge Two Sorted Lists.
+Memory Usage: 14.8 MB, less than 82.97% of C++ online submissions for Merge Two Sorted Lists.
+*/
