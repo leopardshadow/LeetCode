4cbb6ea0d457e39cedda49e66ebc067f8239c7d4
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-03-08 10:03:48 +0800

    141. Linked List Cycle

--- e141.cpp
+++ e141.cpp
@@ -81,3 +81,28 @@ public:
 Runtime: 4 ms, faster than 99.68% of C++ online submissions for Linked List Cycle.
 Memory Usage: 8 MB, less than 58.64% of C++ online submissions for Linked List Cycle.
 */
+
+
+
+
+
+//******************************************************************************//
+// 2022.3.8
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        int len = 0;
+        while(head) {
+            if(++len > 10000)
+                return true;
+            head = head->next;
+        }
+        return false;
+    }
+};
+
+/*
+Runtime: 8 ms, faster than 95.50% of C++ online submissions for Linked List Cycle.
+Memory Usage: 8.1 MB, less than 58.87% of C++ online submissions for Linked List Cycle.
+*/
