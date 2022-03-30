5e3b0efe6b0797de75b2abb23a588703066fcae2
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-03-31 00:36:28 +0800

    9. Palindrome Number

---
+++ e9.py
@@ -0,0 +1,9 @@
+
+# 無聊用 python 寫一下 XD
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
+    
+# Runtime: 97 ms, faster than 42.25% of Python3 online submissions for Palindrome Number.
+# Memory Usage: 13.9 MB, less than 18.58% of Python3 online submissions for Palindrome Number.
