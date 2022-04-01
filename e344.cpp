311f860f4fba2803e9bbcf14ff7ce5676d50014d
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-04-01 15:06:46 +0800

    344. Reverse String

--- e344.cpp
+++ e344.cpp
@@ -6,9 +6,23 @@ void reverseString(char* s, int sSize){
         s[sSize - 1 - i] = c;
     }
 }
-
-
 /*
 Runtime: 52 ms, faster than 28.66% of C online submissions for Reverse String.
 Memory Usage: 12.4 MB, less than 63.36% of C online submissions for Reverse String.
 */
+
+
+//******************************************************************************//
+// 2022.4.1
+
+/*
+愚人節 Leetcode 怎麼沒有特別活動呢 XD
+*/
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        for(int i = 0; i < s.size() / 2; i++)
+            swap(s[i], s[s.size() - 1 - i]);
+    }
+};
