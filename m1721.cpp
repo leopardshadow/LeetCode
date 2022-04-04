9457543e3148277b59e72aa411810d1bbc78b4b3
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-04-04 10:19:26 +0800

    1721. Swapping Nodes in a Linked List

--- m1721.cpp
+++ m1721.cpp
@@ -56,3 +56,38 @@ struct ListNode* swapNodes(struct ListNode* head, int k){
 Runtime: 244 ms, faster than 18.22% of C online submissions for Swapping Nodes in a Linked List.
 Memory Usage: 48.5 MB, less than 19.92% of C online submissions for Swapping Nodes in a Linked List.
 */
+
+
+
+
+
+//******************************************************************************//
+// 2022.4.4
+
+/*
+上一次寫是快一年前了耶，那時候在準備實習面試
+---
+因為這題是 1-indexed，所以要特別注意範圍！！
+*/
+
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode *kthFromHead = head;
+        for (int i = 1; i < k; i++)  // zero indexed!!
+            kthFromHead = kthFromHead->next;
+                
+        ListNode *kthFromTail = head, *temp = head;
+        
+        for (int i = 0; i < k; i++)
+            temp = temp->next;
+
+        while (temp) {
+            temp = temp->next;
+            kthFromTail = kthFromTail->next;
+        }
+                
+        swap(kthFromHead->val, kthFromTail->val);
+        return head;
+    }
+};
