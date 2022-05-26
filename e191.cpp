0678eb0673ab955263f912b2462124705c791ac1
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-05-26 10:22:55 +0800

    191. Number of 1 Bits

--- e191.cpp
+++ e191.cpp
@@ -23,3 +23,28 @@ public:
 Runtime: 0 ms, faster than 100.00% of C++ online submissions for Number of 1 Bits.
 Memory Usage: 5.9 MB, less than 47.57% of C++ online submissions for Number of 1 Bits.
 */
+
+
+
+
+//  2022.5.26
+
+/*
+和之前的寫法一模一樣誒 XD
+*/
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ans = 0;
+        while (n) {
+            n = n & (n-1);
+            ans++;
+        }
+        return ans;
+    }
+};
+/*
+Runtime: 0 ms, faster than 100.00% of C++ online submissions for Number of 1 Bits.
+Memory Usage: 5.9 MB, less than 79.17% of C++ online submissions for Number of 1 Bits.
+*/
