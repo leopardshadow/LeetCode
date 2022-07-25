1df9d22088d96ca4749013dd8ae2ac4b6b24910d
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-07-25 21:36:12 +0800

    34. Find First and Last Position of Element in Sorted Array

--- m34.cpp
+++ m34.cpp
@@ -52,3 +52,51 @@ public:
         return ans;
     }   
 };
+
+
+
+
+/*
+[l, r)
+基本型：f f f f | t t t
+*/
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans(2, -1);
+        if (nums.size() == 0)
+            return ans;
+        
+        // find left boundary
+        int l = 0, r = nums.size(), m;
+        while (l < r) {
+            m = (l + r) / 2;
+            if (nums[m] >= target)
+                r = m;
+            else
+                l = m + 1;
+        }
+        l = min((int)nums.size() - 1, l);
+        if (nums[l] == target)
+            ans[0] = l;
+        
+        // find right boundary
+        l = 0, r = nums.size();
+        while (l < r) {
+            m = (l + r) / 2;
+            if (nums[m] > target)
+                r = m;
+            else
+                l = m + 1;
+        }
+        if (l - 1 >= 0 && nums[l-1] == target)
+            ans[1] = l - 1;
+
+        return ans;
+    }
+};
+/*
+Runtime: 10 ms, faster than 73.39% of C++ online submissions for Find First and Last Position of Element in Sorted Array.
+Memory Usage: 13.7 MB, less than 17.99% of C++ online submissions for Find First and Last Position of Element in Sorted Array.
+*/
