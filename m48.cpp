0599fc891fbe73a521c0889225d488261ac43c41
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-08-30 21:40:10 +0800

    48. Rotate Image

--- m48.cpp
+++ m48.cpp
@@ -65,3 +65,27 @@ int main(int argc, char const *argv[])
 
     return 0;
 }
+
+//******************************************************************************//
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int tmp;
+        int n = matrix.size();
+        for (int i = 0; i < (n+1)/2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = tmp;
+            }
+        }
+    }
+};
+/*
+Runtime: 7 ms, faster than 17.34% of C++ online submissions for Rotate Image.
+Memory Usage: 7.2 MB, less than 33.50% of C++ online submissions for Rotate Image.
+*/
+
+
