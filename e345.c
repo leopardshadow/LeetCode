c811d5d8e400c169fdd4386807ea9b22d7762f36
Cheng-Cheng Lo <thurtu811@gmail.com>
2022-11-04 21:01:02 +0800

    345. Reverse Vowels of a String

--- e345.c
+++ e345.c
@@ -34,3 +34,41 @@ char * reverseVowels(char * s){
 Runtime: 4 ms, faster than 68.06% of C online submissions for Reverse Vowels of a String.
 Memory Usage: 6.8 MB, less than 41.67% of C online submissions for Reverse Vowels of a String.
 */
+
+
+
+
+// 2022.11.4
+
+/*
+原來我之前寫過這題，完全沒印象
+*/
+
+class Solution {
+    bool isVowel(char c) {
+        if (c >= 'A' && c <= 'Z')
+            c = c - 'A' + 'a';
+        return c == 'a' || c == 'e' || c == 'i' ||
+                c == 'o' || c == 'u';
+    }
+public:
+    string reverseVowels(string s) {
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (!isVowel(s[i]))
+                i++;
+            if (!isVowel(s[j]))
+                j--;
+            if (isVowel(s[i]) && isVowel(s[j])) {
+                swap(s[i], s[j]);
+                i++;
+                j--;
+            }
+        }
+        return s;
+    }
+};
+/*
+Runtime: 20 ms, faster than 32.93% of C++ online submissions for Reverse Vowels of a String.
+Memory Usage: 7.7 MB, less than 61.66% of C++ online submissions for Reverse Vowels of a String.
+*/
